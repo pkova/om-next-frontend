0884b431f5f1912bf0c5e1cb87589b562e66fd71
Peter Kovanen <kovanen.peter@gmail.com>
2016-10-26 22:56:35 -0700

    Fix bug in div that covers video

--- src/roy/core.cljs
+++ src/roy/core.cljs
@@ -5,13 +5,13 @@
 
 (defn nav []
   (dom/header #js {:className "header"
-                   :style #js{:textAlign "center"
-                              :width "100%"
-                              :height "50%"
-                              :font-family "Open Sans"
-                              :position "relative"
-                              :display "flex"
-                              :justifyContent "center"}}
+                   :style #js {:textAlign "center"
+                               :width "100%"
+                               :height "50%"
+                               :font-family "Open Sans"
+                               :position "relative"
+                               :display "inline-block"
+                               :justifyContent "center"}}
 
               (dom/div #js {:style #js {:position "absolute"
                                         :width "100%"
@@ -19,26 +19,34 @@
                                         :margin-bottom "auto"
                                         :background "rgba(0, 0, 0, 0.6)"}})
 
-              (dom/video #js {:src "https://12-lvl3-pdl.vimeocdn.com/01/3756/0/18782955/37951023.mp4?expires=1477449450&token=04890934395e7354e482b"
+              (dom/video #js {:src "./assets/37951023.mp4"
                               :autoPlay true
                               :muted true
-                              :style #js {:position "absolute"
+                              :style #js {:position "relative"
                                           :zIndex -1
-                                          :width "100%"}})
+                                          :width "100%"
+                                          :display "block"}})
 
               (dom/img #js {:className "logo",
                             :src "assets/gf-logo.png"
                             :style #js {:position "absolute"
-                                        :transform "scale(0.5)"}})
+                                        :width "25%"
+                                        :left "39%"
+                                        :top "11%"}})
 
               (dom/h1 #js {:style #js {:color "white"
+                                       :fontSize "6vw"
+                                       :fontWeight "lighter"
                                        :position "absolute"
-                                       :top "3em"}}
+                                       :left "33%"
+                                       :top "27%"}}
                       "#this-vs-that")
 
               (dom/h2 #js {:style #js {:color "white"
+                                       :fontSize "2vw"
                                        :position "absolute"
-                                       :top "8em"}}
+                                       :left "41%"
+                                       :top "50%"}}
                       "GETFEEDBACK EATS")))
 
 (defui App
