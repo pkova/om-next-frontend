20c9df52c2455b5ed109e966f7c5fe5480884c98
Peter Kovanen <kovanen.peter@gmail.com>
2016-10-30 17:54:58 -0700

    Upgrade clojurescript to newest version in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject roy "0.1.0-SNAPSHOT"
   :description "Clojurescript frontend with Instafeed API integration"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.9.293"]
                  [org.omcljs/om "1.0.0-alpha34"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
